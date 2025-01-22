d5cc83efb9dd4acf2cfee3ab11967a8c39b5c04f
Panya Seth Ou <122097534+Asetou@users.noreply.github.com>
2025-01-21 23:01:39 -0800

    added angular. CORS error for local.
    Can probably ignore the local get, and wait for the actual thing but who knows

--- API/Program.cs
+++ API/Program.cs
@@ -15,6 +15,12 @@ builder.Services.AddDbContext<DataContext>( opt =>
 
 var app = builder.Build();
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http:localhost:4200","https:localhost:4200"));
+
+
+// Enable the CORS policy
+app.UseCors("AllowAngularApp");
+
 // Configure the HTTP request pipeline.
 app.MapControllers();
 

7add1185d3e867a109342e4062ae2c21cfe4d7bb
Panya Seth Ou <122097534+Asetou@users.noreply.github.com>
2025-01-23 23:13:18 -0800

    registering works with front end now

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,24 +1,20 @@
 import { NgFor } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { AccountService } from './_services/account.service';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgFor, NavComponent],
+  imports: [RouterOutlet, NgFor, NavComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-  http = inject(HttpClient)
   private accountService = inject(AccountService)
-  title = 'Dating App';
-  users: any;
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
   }
 
@@ -30,11 +26,5 @@ export class AppComponent implements OnInit {
     this.accountService.currentUser.set(user);
   }
 
-  getUsers(){
-    this.http.get('http://localhost:5000/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log("Request has been completed")
-    })
-  }
+
 }

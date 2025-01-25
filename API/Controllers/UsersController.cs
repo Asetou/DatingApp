bf752fc4a2d3a72e76ba20c0129bec196c2da2f6
Panya Seth Ou <122097534+Asetou@users.noreply.github.com>
2025-01-25 10:15:50 -0800

    db functionality, updating profiles, loading screen

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections;
+using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
 using API.Data;
 using API.DTOs;
 using API.Entities;
@@ -12,7 +14,7 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Controllers;
 
 [Authorize]
-public class UsersController(IUserRepository userRepository) : BaseApiController{
+public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController{
 
     [HttpGet]
     public async Task<ActionResult<IEnumerable<MemberDTO>>> GetUsers() {
@@ -31,5 +33,22 @@ public class UsersController(IUserRepository userRepository) : BaseApiController
         if (user == null) return NotFound(); 
 
         return user;
+
+
 }
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateDTO memberUpdateDTO){
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        if (username == null) return BadRequest("No username found in token");
+        
+        var user = await userRepository.GetUserByUsernameAsync(username);
+
+        if (user == null) return BadRequest("Could not find user");
+
+        mapper.Map(memberUpdateDTO, user);
+
+        if (await userRepository.SaveAllAsync()) return NoContent();
+
+        return BadRequest("Failed to update the user");
+    }
 }
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,10 +3,13 @@ import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { AccountService } from './_services/account.service';
+import { NgxSpinnerComponent } from 'ngx-spinner';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent],
+  standalone: true,
+  imports: [RouterOutlet, NavComponent, HomeComponent, NgxSpinnerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
